5e127286ae5ae6781fee1a536625a0c02a58b89b
Paul Boeck <paul.boeck@cgi.com>
2018-02-04 16:22:57 +0100

    Add basic question model

---
+++ src/models/question.spec.ts
@@ -0,0 +1,32 @@
+import { Question } from './question';
+
+describe('Question', () => {
+    const question = 'question';
+    const correctAnswer = 'correctAnswer';
+    const wrongAnswers = ['wrongAnswer1', 'wrongAnswer2', 'wrongAnswer3'];
+
+    const q = new Question(question, correctAnswer, wrongAnswers);
+
+    it('Can be created', () => {
+        expect(q).toBeDefined();
+    });
+
+    it('Returns wrong and correct answers', () => {
+        const answers = q.getShuffledAnswers();
+        expect(answers.length).toBe(wrongAnswers.length + 1);
+        wrongAnswers.forEach(a => expect(answers.indexOf(a) >= 0));
+        expect(answers.indexOf(correctAnswer) >= 0);
+    });
+
+    it('Validates correct answer', () => {
+        expect(q.validate(correctAnswer)).toBeTruthy();
+    });
+
+    it('Does not validates wrong answer', () => {
+        wrongAnswers.forEach(a => expect(q.validate(a)).toBeFalsy());
+    });
+
+    it('Returns two wrong answers for 50:50', () => {
+        q.getTwoWrongAnswers().forEach(a => expect(q.validate(a)).toBeFalsy());
+    });
+});
---
+++ src/models/question.ts
@@ -0,0 +1,31 @@
+export class Question {
+    constructor(private _question: string,
+        private _correctAnswer: string,
+        private _wrongAnswers: string[]
+    ) { }
+
+    /**
+    * Shuffles array in place.
+    * @param {Array} a items An array containing the items.
+    * Taken from https://stackoverflow.com/a/6274381/912189
+    */
+    private static shuffle(a) {
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a;
+    }
+
+    getShuffledAnswers(): string[] {
+        return Question.shuffle([this._correctAnswer, ...this._wrongAnswers]);
+    }
+
+    validate(answer: string): boolean {
+        return answer === this._correctAnswer;
+    }
+
+    getTwoWrongAnswers(): any {
+        return Question.shuffle([...this._wrongAnswers]).slice(2);
+    }
+}
